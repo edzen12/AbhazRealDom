abdfbf738e2786101cf9bfde4cc5c0bd35bcba8a
edzen12 <oichiev.edzen@gmail.com>
2021-05-23 18:30:42 +0600

    add new application

--- abhaz/settings.py
+++ abhaz/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
+    'contact',
 ]
 
 MIDDLEWARE = [
---
+++ contact/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+# from .views import ContactForm
+
+
+urlpatterns = [
+    # path('', ContactForm, name="ContactForm")    
+]
