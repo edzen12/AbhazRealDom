3e4fd117c32a448e97920ad5e72d64c236ea2b0c
edzen12 <oichiev.edzen@gmail.com>
2021-08-17 17:53:04 +0600

    edit price filter

--- abhaz/settings.py
+++ abhaz/settings.py
@@ -134,4 +134,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'oichiev.edzen@gmail.com'
-EMAIL_HOST_PASSWORD = 'mmgjdfiktgbcfwbt'
+EMAIL_HOST_PASSWORD = 'gmxjlkmykneuhalp'
